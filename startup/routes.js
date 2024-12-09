13ae0f31f5fdbee30564a5e74ec907a17eb47e49
coderAndrew37 <omolloandrew37@gmail.com>
2024-12-09 15:32:42 +0300

    Backend setup

---
+++ startup/routes.js
@@ -0,0 +1,16 @@
+const products = require("../routes/products.js");
+const auth = require("../routes/auth.js");
+const cart = require("../routes/cart.js");
+const passwordReset = require("../routes/passwordReset.js");
+const contacts = require("../routes/contacts.js");
+const newsletter = require("../routes/newsletter.js");
+const orders = require("../routes/orders.js");
+module.exports = function (app) {
+  app.use("/api/products", products);
+  app.use("/api/users", auth);
+  app.use("/api/cart", cart);
+  app.use("/api/password-reset", passwordReset);
+  app.use("/api/contacts", contacts);
+  app.use("/api/newsletter", newsletter); // Added the newsletter route
+  app.use("/api/orders", orders);
+};
